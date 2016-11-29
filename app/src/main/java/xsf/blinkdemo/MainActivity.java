292f963650191f30076809da7cc9fee7db1145d9
xsf <xsf_uestc_ncl@163.com>
2016-11-29 17:39:23 +0800

    :art: 增加本文点击效果

--- app/src/main/java/xsf/blinkdemo/MainActivity.java
+++ app/src/main/java/xsf/blinkdemo/MainActivity.java
@@ -10,7 +10,7 @@ import xsf.explosion.ExplosionView;
 import xsf.explosion.factory.ParticleFactory;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView tvHello;
+    private TextView tvFireWorks, tvNormal, tvFlyLeft, tvFlyRight, tvFall, tvInnerExplosion, tvFrieWorks2;
     private ImageView ivVx, ivQQ, ivQQmusic, ivQQZone, ivWb, ivFirfox;
     private View layout;
     private ExplosionView explosionView;
@@ -24,32 +24,43 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void init() {
-        tvHello = (TextView) findViewById(R.id.tvHello);
-        ivVx = (ImageView) findViewById(R.id.ivVx);
-        ivQQ = (ImageView) findViewById(R.id.ivQQ);
-        ivQQmusic = (ImageView) findViewById(R.id.ivQQMusic);
-        ivQQZone = (ImageView) findViewById(R.id.ivQQZone);
-        ivWb = (ImageView) findViewById(R.id.ivWb);
-        ivFirfox = (ImageView) findViewById(R.id.ivFirfox);
+        tvFireWorks = (TextView) findViewById(R.id.tvFireWorks);
         layout = findViewById(R.id.layout);
+        tvNormal = (TextView) findViewById(R.id.tvNormal);
+        ivVx = (ImageView) findViewById(R.id.ivVx);
+        tvFlyLeft = (TextView) findViewById(R.id.tvFlyLeft);
+        ivQQ = (ImageView) findViewById(R.id.ivQQ);
+        tvFlyRight = (TextView) findViewById(R.id.tvFlyRight);
+        ivQQmusic = (ImageView) findViewById(R.id.ivQQMusic);
+        tvFall = (TextView) findViewById(R.id.tvFall);
+        ivQQZone = (ImageView) findViewById(R.id.ivQQZone);
+        tvInnerExplosion = (TextView) findViewById(R.id.tvInnerExplosion);
+        ivWb = (ImageView) findViewById(R.id.ivWb);
+        tvFrieWorks2 = (TextView) findViewById(R.id.tvFireWorks2);
+        ivFirfox = (ImageView) findViewById(R.id.ivFirfox);
 
 
-        explosionView = new ExplosionView(this, new ParticleFactory(ParticleFactory.NORMAL));
-        explosionView.addExplosionListener(tvHello);
-        explosionView = new ExplosionView(this, new ParticleFactory(ParticleFactory.NORMAL));
-        explosionView.addExplosionListener(ivVx);
-        explosionView = new ExplosionView(this, new ParticleFactory(ParticleFactory.FLY_LEFT));
-        explosionView.addExplosionListener(ivQQ);
-        explosionView = new ExplosionView(this, new ParticleFactory(ParticleFactory.FLY_RIGHT));
-        explosionView.addExplosionListener(ivQQmusic);
-        explosionView = new ExplosionView(this, new ParticleFactory(ParticleFactory.FALL));
-        explosionView.addExplosionListener(ivQQZone);
-        explosionView = new ExplosionView(this, new ParticleFactory(ParticleFactory.INNER_EXPLOSION));
-        explosionView.addExplosionListener(ivWb);
-        explosionView = new ExplosionView(this, new ParticleFactory(ParticleFactory.FIREWORKS));
-        explosionView.addExplosionListener(ivFirfox);
         explosionView = new ExplosionView(this, new ParticleFactory(ParticleFactory.FIREWORKS));
         explosionView.addExplosionListener(layout);
+        explosionView.addExplosionListener(tvFireWorks);
+        explosionView = new ExplosionView(this, new ParticleFactory(ParticleFactory.NORMAL));
+        explosionView.addExplosionListener(tvNormal);
+        explosionView.addExplosionListener(ivVx);
+        explosionView = new ExplosionView(this, new ParticleFactory(ParticleFactory.FLY_LEFT));
+        explosionView.addExplosionListener(tvFlyLeft);
+        explosionView.addExplosionListener(ivQQ);
+        explosionView = new ExplosionView(this, new ParticleFactory(ParticleFactory.FLY_RIGHT));
+        explosionView.addExplosionListener(tvFlyRight);
+        explosionView.addExplosionListener(ivQQmusic);
+        explosionView = new ExplosionView(this, new ParticleFactory(ParticleFactory.FALL));
+        explosionView.addExplosionListener(tvFall);
+        explosionView.addExplosionListener(ivQQZone);
+        explosionView = new ExplosionView(this, new ParticleFactory(ParticleFactory.INNER_EXPLOSION));
+        explosionView.addExplosionListener(tvInnerExplosion);
+        explosionView.addExplosionListener(ivWb);
+        explosionView = new ExplosionView(this, new ParticleFactory(ParticleFactory.FIREWORKS));
+        explosionView.addExplosionListener(tvFrieWorks2);
+        explosionView.addExplosionListener(ivFirfox);
 
 
         //explosionView.addExplosionListener(layout2);
